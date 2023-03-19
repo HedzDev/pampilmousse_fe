066ea3f613898ac5f346e6d47a506a1dfdeddbb9
Hedi Rivas <hedi.rivas@gmail.com>
2023-03-19 23:53:37 +0100

    changes fetch url whith vercel backend url

--- containers/Home.tsx
+++ containers/Home.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 
   //Fetch places from API
   useEffect(() => {
-    fetch('http://friendly-be.vercel.app/places/getPlaces')
+    fetch('https://friendly-be.vercel.app/places/getPlaces')
       .then((res) => res.json())
       .then((data) => {
         setPlaces(data.places);
